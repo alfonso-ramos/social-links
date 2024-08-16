419f5f4a46757d7bf1ae70d9cbaca3baa15fae41
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-08-16 00:05:48 -0700

    [ADD] Primer commit, base del proyecto

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			"grey-900": "#141414",
+			"grey-800": "#1f1f1f",
+			"grey-700": "#333",
+			"green-700": "#C4F82A",
+			"white": "#fff",
+		},
+		extend: {},
+	},
+	plugins: [],
+}
